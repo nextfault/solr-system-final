150690274b81109160a24ba98d1cfa035a445aea
nextfault <282161603@qq.com>
2017-03-09 09:13:38 +0800

    update
    Signed-off-by: steven <282161603@qq.com>

---
+++ src/main/java/input/index/IndexBuilder.java
@@ -0,0 +1,61 @@
+package input.index;
+
+import input.datasource.DataProviderManager;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.SolrInputDocument;
+import shared.models.Record;
+import solr.MySolrClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Steven's on 2017/3/4.
+ */
+public class IndexBuilder {
+
+    private static long indexCount = 0;
+    private HttpSolrClient solrClient = null;
+
+
+    IndexBuilder(){
+        solrClient = MySolrClient.getInstance().getClient();
+    }
+
+    public static void main(String[] args) {
+        IndexBuilder indexBuilder = new IndexBuilder();
+        indexBuilder.build();
+
+    }
+
+
+    boolean build(){
+        List<Record> records = DataProviderManager.getRecordList();
+        List<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
+
+        for (Record record : records) {
+            docs.add(parse2SolrDoc(record));
+        }
+
+        try {
+            solrClient.add(docs);
+            solrClient.commit();
+            return true;
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return false;
+    }
+
+    SolrInputDocument parse2SolrDoc(Record record){
+        SolrInputDocument solrInputDocument = new SolrInputDocument();
+        solrInputDocument.addField("id",indexCount++);
+        solrInputDocument.addField("recordType",record.getRecordType().getValue());
+        solrInputDocument.addField("context",record.getContext());
+        return solrInputDocument;
+    }
+}
---
+++ src/main/java/solr/MySolrClient.java
@@ -0,0 +1,26 @@
+package solr;
+
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+
+/**
+ * Created by Steven's on 2017/3/9.
+ */
+public class MySolrClient {
+
+    private static final String URL="http://localhost:8983/solr/basic_core";
+    private static MySolrClient instance = new MySolrClient();
+    private HttpSolrClient client = null;
+
+    public static MySolrClient getInstance() {
+        return instance;
+    }
+
+    private MySolrClient() {
+        client = new HttpSolrClient(URL);
+        client.setConnectionTimeout(3000);
+    }
+
+    public HttpSolrClient getClient(){
+        return client;
+    }
+}
